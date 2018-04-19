643ed2d31f5af7a21f8b0346a945572f2e4ef089
Mohammad Mehdi Samsami <msamsami@shirazu.ac.ir>
2018-04-19 10:14:28 +0430

    Update digitRecognition.m

--- digitRecognition.m
+++ digitRecognition.m
@@ -56,7 +56,7 @@ fprintf('\nTraining Set Accuracy: %f\n', acc);
 
 %% ---------------------------------------------------------------------------------
 
-%%%%%%%% PART2: Using Feedforward Propagation in a Pre-trained Neural Network
+%%%%%%%% PART2: Using Feedforward Propagation for a Pre-trained Neural Network
 
 %clc;
 %close all;
@@ -80,4 +80,79 @@ pred2 = NNpredict(Theta1, Theta2, inputMatrix);
 
 % Finally, we find the accuracy of our classifier
 acc2 = mean(double(pred2 == y)) * 100;
-fprintf('\nTraining Set Accuracy (Neural Network Method): %f\n', acc2);
+fprintf('\nTraining Set Accuracy (Pre-trained Neural Network Method): %f\n', acc2);
+
+%% ---------------------------------------------------------------------------------
+
+%%%%%%%% PART3: Using Backpropagation for Neural Network Learning
+
+%clc;
+%close all;
+clear;
+
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+hidden_layer_size = 25;   % 25 Hidden units
+num_labels = 10;  % 10 labels, from 1 to 10 (we have mapped "0" to label "10")
+
+load('TrainingData.mat');  % Loading training data (they will be stored in arrays X, y)
+m = size(X, 1);  % Number of training examples
+
+% Loading some pre-initialized neural network parameters.
+load('NNweights.mat');  % Weights will be loaded into variables Theta1 and Theta2
+
+nn_params = [Theta1(:) ; Theta2(:)];  % Unrolling parameters
+
+
+lambda = 1;  % Weight regularization parameter (we set this to 1 here)
+
+% Computing the cost
+J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lambda); 
+
+% Initializing pameters
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];  % Unrolling initialized parameters
+
+checkNNGradients;  % Checking gradients by running checkNNGradients function
+
+% Once we made sure that backpropagation implementation is correct,
+% we should continue to implement the regularization with the cost and gradient.
+lambda = 3;
+checkNNGradients(lambda);  % Checking gradients by running checkNNGradients function
+
+% Also we output the costFunction debugging values
+debug_J  = nnCostFunction(nn_params, input_layer_size, ...
+                          hidden_layer_size, num_labels, X, y, lambda);  
+           
+           
+% We have now implemented all the code necessary to train a neural 
+%  network. To train your neural network, we will now use "fmincg", which
+%  is a function which works similarly to "fminunc". These advanced optimizers
+%  are able to train our cost functions efficiently as long as we provide
+%  them with the gradient computations.
+
+options = optimset('MaxIter', 75);  % Maximum number of iterations
+
+lambda = 1;  % We should also try different values of lambda
+
+% Creating "short hand" for the cost function to be minimized
+costFunction = @(p) nnCostFunction(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
+
+% Now, costFunction is a function that takes in only one argument (the
+% neural network parameters)
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+                 
+% After training the neural network, we would like to use it to predict
+% the labels. This lets us compute the training set accuracy.
+pred = predict(Theta1, Theta2, X);
+fprintf('\nTraining Set Accuracy (Backpropagation Method): %f\n', mean(double(pred == y)) * 100);
