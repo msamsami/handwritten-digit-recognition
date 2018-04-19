f0464facee47776294c3a3e131ec60b9b9c71ca5
Mohammad Mehdi Samsami <msamsami@shirazu.ac.ir>
2018-04-19 10:34:24 +0430

    Update randInitializeWeights.m

--- randInitializeWeights.m
+++ randInitializeWeights.m
@@ -1,6 +1,6 @@
 function W = randInitializeWeights(L_in, L_out)
-%   This function randomly initializes the weights of a layer with L_in
-%   incoming connections and L_out outgoing connections.
-epsiloninit = 0.12;
-W = rand(L_out, 1 + L_in) * 2 * epsiloninit - epsiloninit;
+    %  This function randomly initializes the weights of a layer with L_in
+    %  incoming connections and L_out outgoing connections.
+    epsiloninit = 0.12;
+    W = rand(L_out, 1 + L_in) * 2 * epsiloninit - epsiloninit;
 end
